246f18324af4e6df42d94fb4474bbac1a4c71daa
Mongy <mongidlanjwa@gmail.com>
2023-07-01 14:43:19 +0200

    fewHomeChanges

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,6 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container-fluid">
+    <img src="https://i.postimg.cc/Hx6j4fYm/vue-P-background.png" alt="background" loading="lazy">
   </div>
 </template>
 
