4438ce29d7279ea4b8bbf7e0dee2c4ed4b7a8616
mongikazi <mongidlanjwa@gmail.com>
2023-07-02 08:27:49 +0200

    about

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,85 @@
 <template>
-  <div class="img">
-    <h1>This is an about page</h1>
-  </div>
+    <div class= "container-fluid">
+        div class="container-left">
+            <div class="profile">
+                <img src=" ">
+                <h1>Aspiring ..</h1>
+                <p></p>
+            </div>
+        </div>
+    </div>
+
+    <div class="social-icons">
+        <div class="icons">
+            <a href="">
+             img
+            <h2>Address</h2>
+        </div>
+        <div class="icons">
+            <a href="">
+             img
+            <h2>Email</h2>
+        </div>
+        <div class="icons">
+            <a href="">
+              img
+            <h2>Call</h2>
+        </div>
+        <div class="icons">
+            <a href="">
+                img
+            <h2>WhatsApp</h2>
+        </div>
+    </div>
+
+    <div class="container-right">
+        <div class="about">
+            <h1>About me</h1>
+            <p> </p>
+        </div>
+    </div>
+
+
+    div class="skills-section">
+        <h1>Skills</h1>
+            <div class="logo>
+            
+            </div>
+    
+    </div>
 </template>
+
+<script>
+
+    export default {
+    name: 'ProductsView',
+    components: {
+    
+  }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+.container-fluid{
+    display: flex,
+    flex-direction: row,
+    background-color: black
+}
+
+.container-right{
+    background-color: brown,
+    padding: 3%,
+    width: 30%
+}
+
+.container-left{
+    background-color: black,
+    margin: 2.5%
+    width: 70%,
+}
+
+.profile{
+  borderz; 5rem
+}
+</style>
