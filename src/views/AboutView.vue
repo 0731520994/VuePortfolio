0558ecbaf3cb2bd8b9164472c9509999f2ada60b
mongikazi <mongidlanjwa@gmail.com>
2023-07-02 19:37:07 +0200

    secondApush

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,58 +1,18 @@
+
 <template>
-    <div class= "container-fluid">
-        div class="container-left">
-            <div class="profile">
-                <img src=" ">
-                <h1>Aspiring ..</h1>
-                <p></p>
-            </div>
-        </div>
-    </div>
-
-    <div class="social-icons">
-        <div class="icons">
-            <a href="">
-             img
-            <h2>Address</h2>
-        </div>
-        <div class="icons">
-            <a href="">
-             img
-            <h2>Email</h2>
-        </div>
-        <div class="icons">
-            <a href="">
-              img
-            <h2>Call</h2>
-        </div>
-        <div class="icons">
-            <a href="">
-                img
-            <h2>WhatsApp</h2>
-        </div>
-    </div>
-
-    <div class="container-right">
-        <div class="about">
-            <h1>About me</h1>
-            <p> </p>
-        </div>
-    </div>
-
 
     div class="skills-section">
         <h1>Skills</h1>
             <div class="logo>
             
             </div>
-    
     </div>
 </template>
 
 <script>
 
     export default {
-    name: 'ProductsView',
+    name: 'AboutView',
     components: {
     
   }
@@ -61,25 +21,19 @@
 
 <style lang="scss" scoped>
 
-.container-fluid{
-    display: flex,
-    flex-direction: row,
-    background-color: black
+.logos img{
+    height: 3rem
 }
 
-.container-right{
-    background-color: brown,
-    padding: 3%,
-    width: 30%
-}
-
-.container-left{
+.button{
     background-color: black,
-    margin: 2.5%
-    width: 70%,
+    margin: 5px
 }
 
-.profile{
-  borderz; 5rem
+.button a{
+    text-decoration: none,
+    color: brown   
 }
+
+
 </style>
