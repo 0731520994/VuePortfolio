0bdf3cc1db02d04c18c36a54d2df764d5679eb59
Mongy <mongidlanjwa@gmail.com>
2023-07-03 16:43:26 +0200

    changes

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,15 +1,38 @@
 <template>
-    <div>
+  
+        <h1>Projects</h1>
+        <div class="container-fluid">
+            <div class="col">
+                <div class="card" style="width: 18rem" v-for="project in projects" :key="project.id">
+                    <img :src="project.image" class="card-img-top" alt="...">
+                  <div class="card-body">
+                    <h5 class="card-title">{{ project.name }}</h5>
+                    <p class="card-text">{{ project.description }}</p>
+                    <a href="#" class="btn btn-primary">Click</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+               
+            
+           
+       
 
-    </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {
+  computed: {
+    projects() {
+      return this.$store.state.projects;
+    },
+  },
+  mounted() {
+    this.$store.dispatch('fetchProjects');
+  },
+};
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
 
 </style>
--- src/views/ResumeView.vue
+++ src/views/ResumeView.vue
@@ -1,15 +1,40 @@
 <template>
-    <div>
-
-    </div>
+    <template>
+        <div>
+          <div class="container-fluid">
+            <div class="row">
+              <div class="col">
+                <div class="card" style="width: 18rem" v-for="resume in resumes" :key="resume.id">
+                  <div class="card-body">
+                    <h5 class="card-title">{{ resume.year }}</h5>
+                    <p class="card-text">{{ resume.description }}</p>
+                    <h6 class="card-subtitle mb-2 text-muted">{{ resume.place }}</h6>
+                    <p class="card-text">{{ resume.type }}</p>
+                    <a :href="resume.certificate" class="card-link">Certificate</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </template>
+      
+   
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {
+  computed: {
+    resume() {
+      return this.$store.state.resume;
+    },
+  },
+  mounted() {
+    this.$store.dispatch('fetchResume');
+  },
+};
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
 
 </style>
